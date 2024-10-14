4e325bac4ca0de3d7f8e27b0cc3540970890b668
LelieL91 <48968075+LelieL91@users.noreply.github.com>
2024-10-14 03:48:50 +0200

    Update install-xui.py

--- install-xui.py
+++ install-xui.py
@@ -93,7 +93,6 @@ if __name__ == "__main__":
     except:
         printc("Creating user")
         os.system("sudo adduser --system --shell /bin/false --group --disabled-login xui >/dev/null 2>&1")
-#        os.system("sudo adduser --system --shell /bin/false xui >/dev/null 2>&1")
     if not os.path.exists("/home/xui >/dev/null 2>&1"): os.mkdir("/home/xui")
     
     ##################################################
